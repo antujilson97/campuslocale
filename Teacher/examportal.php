252db3bded91544138fcd24a5efa252dd7b728b1
antujilson97 <antujilson@gmail.com>
2022-05-30 23:18:12 +0530

    New commit
    Latest Commit

---
+++ Teacher/examportal.php
@@ -0,0 +1,50 @@
+<?php  
+	session_start();
+	if (!$_SESSION["LoginTeacher"])
+	{
+		header('location:../login/login.php');
+	}
+		require_once "../connection/connection.php";
+	?>
+<!---------------- Session Ends form here ------------------------>
+
+
+<!doctype html>
+<html lang="en">
+	<head>
+		<title>Campus Locale</title>
+	</head>
+	<body>
+		<?php include('../common/common-header.php') ?>
+		<?php include('../common/teacher-sidebar.php') ?>  
+
+		<main role="main" class="col-xl-10 col-lg-9 col-md-8 ml-sm-auto px-md-4 main-background mb-2 w-100">
+			<div class="sub-main">
+				<div class="text-center d-flex flex-wrap flex-md-nowrap pt-3 pb-2 mb-3 text-white admin-dashboard pl-3">
+					<h4 class="">Add Exam</h4>
+				</div>
+				<div class="row">
+					<div class="col-md-12">
+						<section class="border mt-3">
+<div class="row">
+<div class="col-md-3"></div>
+<div class="col-md-6">
+<form role="form" method="post" action="../exam/admin.php?q=index.php">
+<div class="form-group">
+<input type="text" name="uname" maxlength="20"  placeholder=" User id" class="form-control"/> 
+</div>
+<div class="form-group">
+<input type="password" name="password" maxlength="15" placeholder="Password" class="form-control"/>
+</div>
+<div class="form-group" align="center">
+<input type="submit" name="login" value="Login" class="btn btn-primary" />
+</div>
+</form>
+</div><div class="col-md-3"></div></div>
+      </div>
+      <!--<div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>-->
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
